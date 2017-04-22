57679986be931f66530cf998b4e8ef7cea39b374
j4k0b0 <jacobo.ortiz.hdz@gmail.com>
2017-04-22 11:59:42 -0700

    Added wordpress loop

--- wp-content/themes/wp-starter/index.php
+++ wp-content/themes/wp-starter/index.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 	<div class="page-wrapper">
 		<?php the_content(); ?>
 	</div>
+
+	<?php endwhile; ?>
+
+<?php endif;?>
 
 <?php get_footer(); ?>
